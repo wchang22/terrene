2b46c26eedc8ca8cf70143d1053b7e12d85ac1af
Wesley Chang <31712902+wchang22@users.noreply.github.com>
2020-06-07 19:47:35 -0700

    Fix WDYR rescript

--- .rescriptsrc.js
+++ .rescriptsrc.js
@@ -1,24 +1,43 @@
 const { edit, getPaths } = require("@rescripts/utilities");
 
-const predicate = valueToTest => {
-  return valueToTest.oneOf;
+function rescriptGlslifyPlugin() {
+  const predicate = valueToTest => {
+    return valueToTest.oneOf;
+  }
+  
+  const transform = match => ({
+    ...match,
+    oneOf: [
+      // Need to add as second-to-last to avoid being intercepted by the file-loader in CRA
+      ...match.oneOf.slice(0, -1),
+      {
+        test: /\.(glsl|frag|vert)$/,
+        exclude: [/node_modules/],
+        use: ["raw-loader", "glslify-loader"]
+      },
+      ...match.oneOf.slice(-1)
+    ]
+  });
+
+  return config => {
+    const matchingPaths = getPaths(predicate, config);
+    return edit(transform, matchingPaths, config);
+  };
 }
 
-const transform = match => ({
-  ...match,
-  oneOf: [
-    // Need to add as second-to-last to avoid being intercepted by the file-loader in CRA
-    ...match.oneOf.slice(0, -1),
-    {
-      test: /\.(glsl|frag|vert)$/,
-      exclude: [/node_modules/],
-      use: ["raw-loader", "glslify-loader"]
+function rescriptWDYRPlugin() {
+  const predicate = valueToTest => {
+    return valueToTest.alias;
+  }
+  
+  const transform = match => ({
+    ...match,
+    alias: {
+      ...match.alias,
+      'react-redux': process.env.NODE_ENV === 'development' ? 'react-redux/lib' : 'react-redux'
     },
-    ...match.oneOf.slice(-1)
-  ]
-});
+  });
 
-function rescriptGlslifyPlugin() {
   return config => {
     const matchingPaths = getPaths(predicate, config);
     return edit(transform, matchingPaths, config);
@@ -29,4 +48,7 @@ module.exports = [
   [
     rescriptGlslifyPlugin,
   ],
+  [
+    rescriptWDYRPlugin,
+  ]
 ]
