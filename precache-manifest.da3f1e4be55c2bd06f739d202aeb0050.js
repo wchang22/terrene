adbc4d234ff532ef99011fef131c4a102054e109
Wesley Chang <31712902+wchang22@users.noreply.github.com>
2020-06-15 00:27:59 -0700

    Updates

---
+++ precache-manifest.da3f1e4be55c2bd06f739d202aeb0050.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b29ec70203e8ec86fa741b70bd4cb881",
+    "url": "/terrene/index.html"
+  },
+  {
+    "revision": "426ba3470724a56c28e9",
+    "url": "/terrene/static/css/main.67e335d2.chunk.css"
+  },
+  {
+    "revision": "4e31594265faf93fc790",
+    "url": "/terrene/static/js/2.4330a59e.chunk.js"
+  },
+  {
+    "revision": "58d5fbafae534608cf42b83027b3e559",
+    "url": "/terrene/static/js/2.4330a59e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "426ba3470724a56c28e9",
+    "url": "/terrene/static/js/main.bc621fe7.chunk.js"
+  },
+  {
+    "revision": "f7ffe098908cfbe3feec",
+    "url": "/terrene/static/js/runtime-main.df4d5e20.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.561456b84014c39f49501e779b86ec57.js"
+  "/terrene/precache-manifest.da3f1e4be55c2bd06f739d202aeb0050.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/terrene/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
